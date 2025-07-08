342371faf43bad4b5c23c83e4ac94f0a9678ddd9
Ourten <3457249708@qq.com>
2025-07-09 01:42:44 +0800

    学习任务生成的插件的提交

--- code/lag1-cosmic-debug/src/main/java/plugins/AI/Learning_Task_calendar.java
+++ code/lag1-cosmic-debug/src/main/java/plugins/AI/Learning_Task_calendar.java
@@ -21,12 +21,12 @@ import java.util.Map;
  */
 public class Learning_Task_calendar extends AbstractBasePlugIn implements Plugin {
 //输入有：学生对知识点方案的平均正确率+该知识点的做题数量+想提升的模式（基础/中等+困难）+ 想要掌握的知识点列表
-@Override
-public void registerListener(EventObject e) {
-    // 注册点击事件
-    super.registerListener(e);
-    this.addItemClickListeners("tbmain");
-}
+    @Override
+    public void registerListener(EventObject e) {
+        // 注册点击事件
+        super.registerListener(e);
+        this.addItemClickListeners("tbmain");
+    }
 
     public void itemClick(ItemClickEvent e) {
 //        this.getView().setEnable(false, "lag1_pointbindcourse");
@@ -34,24 +34,19 @@ public void registerListener(EventObject e) {
         Control source = (Control) e.getSource();
         //改
         if (e.getItemKey().equalsIgnoreCase("lag1_button_knowpoint")) {
-//            else {
-//                 设置第二个微服务按钮不可见和禁用
-//                this.getView().setVisible(false, "lag1_pointbindcourse");
-//            }
             // 获取日任务信息，并且以JSON字符串的形式展现
             JSONObject jsonResultObject = new JSONObject();
             for (int i = 1; i <= 3; i++) {
                 jsonResultObject.put("lag1_chapter" + i , this.getModel().getValue("lag1_chapter" + i).toString());
 
             }//目前没有对小标题分析
-            //----下面是正常提取教材代码----
             // 调用GPT开发平台微服务
             Map<String, String> variableMap = new HashMap<>();
             variableMap.put("courseinfo", jsonResultObject.toJSONString());
 
             Object[] params = new Object[]{
                     //GPT提示编码
-                    getPromptFid("prompt-2505280F653A39"),//改
+                    getPromptFid("prompt-250704BDD64BE5"),
                     "开始输出学习计划方案",
                     variableMap
             };
@@ -66,11 +61,7 @@ public void registerListener(EventObject e) {
             Markdown mk = this.getView().getControl("lag1_md");
             mk.setText(jsonObjectData.getString("llmValue"));
 
-//            if (globalLlmValue != null && !globalLlmValue.isEmpty()) {
-//                // 设置第二个微服务按钮可见和启用
-////                this.getView().setVisible(true, "lag1_pointbindcourse");
-//                this.getView().setEnable(true, "lag1_pointbindcourse");
-//            }
+
         }
 
     }
