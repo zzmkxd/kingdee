55fde31ef74660a8632c35241fec97fdfa1a6f66
Yumi0606 <1015622964@qq.com>
2025-07-17 17:10:29 +0800

    无事发生

--- code/lag1-cosmic-debug/src/main/java/lag1/cosmic/debug/DebugApplication.java
+++ code/lag1-cosmic-debug/src/main/java/lag1/cosmic/debug/DebugApplication.java
@@ -26,10 +26,10 @@ public class DebugApplication {
         cosmic.setTenantNumber("ierp");
         
 //        cosmic.setConfigUrl("127.0.0.1:2181?user=zk&password=zk123456");
-        cosmic.setConfigUrl("10.252.74.112:2181?user=zookeeper&password=zk123456");
+        cosmic.setConfigUrl("127.0.0.1:2181?user=zookeeper&password=zk123456");
         
 //        cosmic.setMcServerUrl("http://127.0.0.1:8090");
-        cosmic.setMcServerUrl("http://10.252.74.112:8071"); 
+        cosmic.setMcServerUrl("http://127.0.0.1:8071"); 
         
 //        cosmic.setFsServerUrl("127.0.0.1", 8100);
 //        cosmic.setImageServerUrl("127.0.0.1", 8100);
