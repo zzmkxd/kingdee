62213f89b062a4ca74978b0978ddbe408b45b344
Yumi0606 <1015622964@qq.com>
2025-08-10 13:25:07 +0800

    备份前端资源

--- server.js
+++ server.js
@@ -3,26 +3,36 @@ const path = require("path");
 const serveIndex = require("serve-index");
 
 const app = express();
-const PORT = 3002;
+const PORT = 3003;
 
-// 设置跨域访问
-app.all('*', function (req, res, next) {
-    // 设置可以跨域访问的域名，比如 http://localhost:8881
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8881');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    // res.header('Content-Type','text/css;test/javascript;text/html;application/json;application/x-img');
+// 允许跨域的白名单
+const allowOrigins = [
+    "http://127.0.0.1:8881",
+    "http://127.0.0.1:3003",
+    "http://10.26.0.2:8072",
+];
+
+app.all("*", function (req, res, next) {
+    const origin = req.headers.origin;
+    if (allowOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+    );
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     next();
 });
 
-// 设置静态资源文件夹
-app.use('/',express.static(path.join(__dirname, 'public')));
+// 静态资源
+app.use("/", express.static(path.join(__dirname, "public")));
 
-// 设置静态资源索引
-app.use('/', serveIndex(path.join(__dirname, 'public'), {'icons': true}));
+// 目录索引
+app.use("/", serveIndex(path.join(__dirname, "public"), { icons: true }));
 
-// 启动服务器
+// 启动
 app.listen(PORT, () => {
-    console.log(`CustomControl Static Server is Running on http://localhost:$ {PORT}`);
+    console.log(`CustomControl Static Server is running on http://localhost:${PORT}`);
 });
